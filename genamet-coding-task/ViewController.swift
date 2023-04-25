b309e07ee1312639562fc9f0f2e021d2ea2278eb
Onkar <onkar@active.agency>
2023-04-25 11:49:45 +0530

    Initial commit

---
+++ genamet-coding-task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  genamet-coding-task
+//
+//  Created by Active Mac Lap 01 on 25/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
